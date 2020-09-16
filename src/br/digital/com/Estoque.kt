c76e9af8324297eae850d1147a2083ae1da021bd
artchier <ar-chaves@hotmail.com>
2020-09-16 10:59:01 -0300

    Criação da classe Estoque com seus atributos e do bloco Init

---
+++ src/br/digital/com/Estoque.kt
@@ -0,0 +1,16 @@
+package br.digital.com
+
+class Estoque (var nome: String, var qtdAtual: Int, var qtdMinima: Int) {
+
+    init {
+        println("Novo estoque criado")
+    }
+    //eu vi que o nome disso aqui é backing field
+    var _qtdAtual: Int = qtdAtual
+        get() = qtdAtual
+        private set
+
+    var _qtdMinima: Int = qtdMinima
+        get() = qtdMinima
+        private set
+}
