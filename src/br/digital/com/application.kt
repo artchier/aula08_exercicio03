ed83a4ace53463c75ca5a63acc8b2beafb3baccf
Ananda Câmara <67615096+anandacamara@users.noreply.github.com>
2020-09-16 18:30:42 -0300

    Update application.kt

--- src/br/digital/com/application.kt
+++ src/br/digital/com/application.kt
@@ -2,10 +2,10 @@ package br.digital.com
 
 fun main(){
 
-    // Criação de estoques de produtos diferentes
+    // Criação do estoque
     val estoque1 = Estoque("Bebidas", 12, 5)
 
-    // Testando a função em que a quantidade mínima é menor que a atual
+    // Testando as funções
     estoque1.mudarQtdMinima(10)
     println(estoque1.precisaRepor())
     estoque1.mostra()
